3bdd9fdd49a98ea61199654524de430807923f95
Sami Greenbury <git@mha.systems>
2019-11-05 15:08:06 +0000

    Renamed ResponseJson to ResponseData (it isn't JSON). Added status codes and methods to AbstractResponse.

--- src/AbstractResponse.php
+++ src/AbstractResponse.php
@@ -6,8 +6,18 @@ use GuzzleHttp\Psr7\Response as GuzzleResponse;
 
 abstract class AbstractResponse
 {
-    protected $response;
-    protected $responseJson;
+    const STATUS_OK = 'Ok'; // Request could be processed as expected.
+    const STATUS_INVALID_URL = 'InvalidUrl'; // URL string is invalid.
+    const STATUS_INVALID_SERVICE = 'InvalidService'; // Service name is invalid.
+    const STATUS_INVALID_VERSION = 'InvalidVersion'; // Version is not found.
+    const STATUS_INVALID_OPTIONS = 'InvalidOptions'; // Options are invalid.
+    const STATUS_INVALID_QUERY = 'InvalidQuery'; // The query string is synctactically malformed.
+    const STATUS_INVALID_VALUE = 'InvalidValue'; // The successfully parsed query parameters are invalid.
+    const STATUS_NO_SEGMENT = 'NoSegment'; // One of the supplied input coordinates could not snap to street segment.
+    const STATUS_TOO_BIG = 'TooBig'; // The request size violates one of the service specific request size restrictions.
+
+    protected $response; // Holds the raw GuzzleResposne object
+    protected $responseData; // StdObject holding the data from $response
     /**
      * @var Client
      */
@@ -19,15 +29,34 @@ abstract class AbstractResponse
     public function __construct(GuzzleResponse $response)
     {
         $this->response = $response;
-        $this->responseJson = \GuzzleHttp\json_decode((string) $response->getBody());
+        $this->responseData = \GuzzleHttp\json_decode((string) $response->getBody());
+    }
+    public function getStatus()
+    {
+        return $this->responseData->code;
+    }
+
+    public function isError()
+    {
+        return !$this->isOk();
+    }
+
+    public function isOk()
+    {
+        return $this->responseData->code === self::STATUS_OK;
+    }
+
+    public function getMessage()
+    {
+        return $this->responseData->message;
     }
 
     public function dumpResponse($echo = true)
     {
         if ($echo) {
-            var_export($this->responseJson);
+            var_export($this->responseData);
         } else {
-            var_export($this->responseJson, true);
+            var_export($this->responseData, true);
         }
     }
 }
