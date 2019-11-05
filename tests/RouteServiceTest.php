4283db32d372c6a8eae3dd2bd0e368a24da33cc2
Sami Greenbury <git@mha.systems>
2019-11-05 15:28:26 +0000

    Setup testing config defaults to use Demo server. Changed demo coordinates to use Berlin (as used in Quick Start demo for custom servers).

--- tests/RouteServiceTest.php
+++ tests/RouteServiceTest.php
@@ -9,19 +9,36 @@ use GuzzleHttp\Exception\ClientException;
 class RouteServiceTest extends TestCase
 {
     /**
+     * In order to be able to test against the demo server, we need to
+     * wait a bit between requests. We don't need to finish our test fasts
+     * but we do need them to be reliable.
+     *
+     * @return void
+     */
+    private function delayBetweenSending()
+    {
+        sleep(5);
+    }
+
+    private function getOsrmConfig()
+    {
+        return [
+            'host' => env('OSRM_HOST', 'router.project-osrm.org'),
+            'port' => env('OSRM_PORT', '80'),
+        ];
+    }
+    /**
      * Check we can make a request, and get an Ok back.
      */
     public function testCanGetDrivingDirections()
     {
-        $routeRequest = new RouteServiceRequest([
-            'host' => 'router.project-osrm.org',
-            'port' => 80
-        ]);
+        $routeRequest = new RouteServiceRequest($this->getOsrmConfig());
         $routeRequest->setCoordinates([
-            [-112.068195, 33.712053],
-            [-112.065295, 33.602053],
-            [-112.023641, 33.626367]
+            [13.428395, 52.537307],
+            [13.452084, 52.509420],
+            [13.401272, 52.511718]
         ]);
+        $this->delayBetweenSending();
         $response = $routeRequest->send();
         $this->assertEquals('Ok', $response->getStatus());
     }
@@ -32,20 +49,45 @@ class RouteServiceTest extends TestCase
      */
     public function testGetAnErrorOnInvalidRequest()
     {
-        $routeRequest = new RouteServiceRequest([
-            'host' => 'router.project-osrm.org',
-            'port' => 80
-        ]);
+        $routeRequest = new RouteServiceRequest($this->getOsrmConfig());
         $routeRequest->setCoordinates([
-            [-112.068195, 33.712053]
+            [13.428395, 52.537307],
         ]);
         try {
+            $this->delayBetweenSending();
             $response = $routeRequest->send();
         } catch (ClientException $e) {
             $this->assertEquals(
                 $e->getResponse()->getStatusCode(),
-                422
+                400
             );
         }
+    }
+
+    /**
+     * Check that if we make an invalid request (a route request with only
+     * one waypoint) get get an error.
+     */
+    public function testGettingDistanceInDifferentUnits()
+    {
+        $routeRequest = new RouteServiceRequest($this->getOsrmConfig());
+        $routeRequest->setCoordinates([
+            [13.358583, 52.514707],
+            [13.451166, 52.509881],
+        ]);
+        $this->delayBetweenSending();
+        $response = $routeRequest->send();
+        /* @var $route \Dmgctrlr\LaraOsrm\Models\Route */
+        $route = $response->getFirstRoute();
+        $this->assertEquals(
+            '11',
+            $route->getDistance(),
+            'Get the distance in the default unit (km)'
+        );
+        $this->assertEquals(
+            '12',
+            $route->getDistance('miles'),
+            'Get the same distance in miles'
+        );
     }
 }
